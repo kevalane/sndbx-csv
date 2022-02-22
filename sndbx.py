b824c214d22035154db44cdfbc5a927cba43412b
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-02-22 21:04:24 +0100

    removed duplicates.

--- sndbx.py
+++ sndbx.py
@@ -2,7 +2,25 @@ import pandas as pd
 
 data = pd.read_csv('Book1.csv')
 
+products = []
+transactions = []
+touched = []
 for i, j in data.iterrows():
-    print(str(i) + ": " + j.productName + ", $" + str(j.transactionAmountUSD))
-    print()
+    products.append(j.productName)
+    transactions.append(j.transactionAmountUSD)
 
+for i in range(0, len(products)):
+    for j in range(i+1, len(products)):
+        if (products[i] == products[j]):
+            transactions[i] = transactions[i] + transactions[j]
+            touched.append(j)
+
+for i in range(0, len(touched)):
+    products.pop(touched[i])
+    transactions.pop(touched[i])
+
+
+for i in range(0, len(products)):
+    print(str(products[i]) + ": " + str(transactions[i]))
+
+input("Press any key to continue...")
